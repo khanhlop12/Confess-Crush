38d4dc2c31157690962e81b61d42fd65a8bbee14
khanhlop12 <104995917+khanhlop12@users.noreply.github.com>
2022-05-05 21:10:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Demo",
-    introTitle: 'Nguyen van A',
+    introTitle: 'Nguyễn Khuyến',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
